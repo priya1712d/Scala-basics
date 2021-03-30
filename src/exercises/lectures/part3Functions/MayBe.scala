156e8218a783d5a57421110dea79cab42e65aaab
priya1712d <priyachawla28@gmail.com>
2021-03-30 13:02:35 +0530

    Scala Basics

--- src/exercises/lectures/part3Functions/MayBe.scala
+++ src/exercises/lectures/part3Functions/MayBe.scala
@@ -9,10 +9,24 @@ abstract class MayBe[+T] {
 
 
 case object MayBeNot extends MayBe[Nothing] {
-
-  def map[]
+  def map[B](f: Nothing => B): MayBe[B] = MayBeNot
+  def flatMap[B](f: Nothing => MayBe[B]): MayBe[B] = MayBeNot
+  def filter(p: Nothing => Boolean): MayBe[Nothing] = MayBeNot
 }
 
 case class Just[+T](value: T) extends MayBe[T] {
+  def map[B](f: T => B): MayBe[B] = Just(f(value))
+  def flatMap[B](f: T => MayBe[B]): MayBe[B] = f(value)
+  def filter(p: T => Boolean): MayBe[T] =
+    if(p(value)) this
+    else MayBeNot
+}
+
+object MayBeTest extends App {
+val just3 = Just(3)
+  println(just3)
+  println(just3.map(_ * 2))
+  println(just3.flatMap( x => Just(x % 2 == 0)))
+  println(just3.filter(_ % 2 == 0))
 
 }
---
+++ src/exercises/lectures/part3Functions/Sequences.scala
@@ -0,0 +1,10 @@
+package exercises.lectures.part3Functions
+
+object Sequences extends App{
+
+  val aSeq:Seq[Int] = 1 until 10
+  aSeq.foreach(println)
+
+
+
+}
